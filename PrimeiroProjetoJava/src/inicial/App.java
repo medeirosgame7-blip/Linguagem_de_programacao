c1a2a8d467edb090c8f661af7f3c8daf8997785a
medeirosgame7-blip <medeirosgame7@gmail.com>
2026-06-02 17:04:31 -0300

    Add files via upload

---
+++ PrimeiroProjetoJava/src/inicial/App.java
@@ -0,0 +1,21 @@
+package inicial;
+
+public class App {
+
+	public static void main(String[] args) {
+		String nome = "Esthevan";
+		int idade = 17;
+		
+		System.out.println("Olá, Mundo!!!"); 
+		System.out.println("Seja bem-vindo " + nome + ". "); 
+		System.out.println("Essa é sua primeira aula de Java!!!"); 
+		
+		System.out.printf("Meu nome é %s e tenho %d anos. \n\n", nome, idade); 
+        
+		double pi = 3.1415926536;
+		
+		System.out.printf("Ovalor de pi é %.2f", pi);
+		
+	}
+
+}
